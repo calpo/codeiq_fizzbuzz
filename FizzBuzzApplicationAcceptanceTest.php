227c5bc979281d80742763c914fc4ac38c221dca
calpo <calpo22@gmail.com>
2013-07-10 11:17:55 +0900

    受け入れテスト追加

---
+++ FizzBuzzApplicationAcceptanceTest.php
@@ -0,0 +1,73 @@
+<?php
+class FizzBuzzApplicationAcceptanceTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+		ob_flush();
+		ob_start();
+    }
+
+	public function tearDown()
+	{
+		ob_end_flush();
+	}
+
+	/**
+	 * @test
+	 */
+    function 受け入れテスト()
+    {
+		$app = new FizzBuzzApplication;
+
+		$app->addSpecAndMessage(new FizzBuzzSpecification(15), 'fizzbuzz');
+		$app->addSpecAndMessage(new FizzBuzzSpecification(3), 'fizz');
+		$app->addSpecAndMessage(new FizzBuzzSpecification(5), 'buzz');
+
+		$data = range(1,30);
+
+		$app->run($data);
+
+		$str = ob_get_flush();
+
+		$this->assertEquals($this->expectedFizzBuzzString(), $str);
+    }
+
+
+    private function expectedFizzBuzzString()
+    {
+        return <<<EOT
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+16
+17
+fizz
+19
+buzz
+fizz
+22
+23
+fizz
+buzz
+26
+fizz
+28
+29
+fizzbuzz
+EOT;
+    }
+
+}
+
